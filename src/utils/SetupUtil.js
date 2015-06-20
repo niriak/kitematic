e02549bff659d9599f1565ed1407f6347bde738e
Guillaume Hain <zedtux@zedroot.org>
2015-06-20 12:28:00 +0200

    Implement Linux support

--- src/utils/SetupUtil.js
+++ src/utils/SetupUtil.js
@@ -2,8 +2,8 @@ import _ from 'underscore';
 import fs from 'fs';
 import path from 'path';
 import Promise from 'bluebird';
-import util from './Util';
 import bugsnag from 'bugsnag-js';
+import util from './Util';
 import virtualBox from './VirtualBoxUtil';
 import setupServerActions from '../actions/SetupServerActions';
 import metrics from './MetricsUtil';
@@ -56,64 +56,56 @@ export default {
     let machineVersion = null;
     while (true) {
       try {
-        setupServerActions.started({started: false});
+        let ip = null;
 
-        // Make sure virtulBox and docker-machine are installed
-        let virtualBoxInstalled = virtualBox.installed();
-        let machineInstalled = machine.installed();
-        if (!virtualBoxInstalled || !machineInstalled) {
-          router.get().transitionTo('setup');
-          if (!virtualBoxInstalled) {
-            setupServerActions.error({error: 'VirtualBox is not installed. Please install it via the Docker Toolbox.'});
-          } else {
-            setupServerActions.error({error: 'Docker Machine is not installed. Please install it via the Docker Toolbox.'});
-          }
-          this.clearTimers();
-          await this.pause();
-          continue;
-        }
-
-        virtualBoxVersion = await virtualBox.version();
-        machineVersion = await machine.version();
-
-        setupServerActions.started({started: true});
-        metrics.track('Started Setup', {
-          virtualBoxVersion,
-          machineVersion
-        });
-
-        let exists = await virtualBox.vmExists(machine.name()) && fs.existsSync(path.join(util.home(), '.docker', 'machine', 'machines', machine.name()));
-        if (!exists) {
-          router.get().transitionTo('setup');
-          setupServerActions.started({started: true});
-          this.simulateProgress(60);
-          try {
-            await machine.rm();
-          } catch (err) {}
-          await machine.create();
+        if (util.isLinux()) {
+          ip = 'localhost';
         } else {
-          let state = await machine.status();
-          if (state !== 'Running') {
-            if (state === 'Saved') {
-              router.get().transitionTo('setup');
-              this.simulateProgress(10);
-            } else if (state === 'Stopped') {
-              router.get().transitionTo('setup');
-              this.simulateProgress(25);
-            }
-            await machine.start();
+          setupServerActions.started({started: false});
+          if (!virtualBox.installed()) {
+            router.get().transitionTo('setup');
+            throw new Error('VirtualBox is not installed. Please install it via the Docker Toolbox.');
           }
-        }
 
-        // Try to receive an ip address from machine, for at least to 80 seconds.
-        let tries = 80, ip = null;
-        while (!ip && tries > 0) {
-          try {
-            console.log('Trying to fetch machine IP, tries left: ' + tries);
-            ip = await machine.ip();
-            tries -= 1;
-            await Promise.delay(1000);
-          } catch (err) {}
+          if (!machine.installed()) {
+            router.get().transitionTo('setup');
+            throw new Error('Docker Machine is not installed. Please install it via the Docker Toolbox.');
+          }
+
+          setupServerActions.started({started: true});
+          let exists = await virtualBox.vmExists(machine.name()) && fs.existsSync(path.join(util.home(), '.docker', 'machine', 'machines', machine.name()));
+          if (!exists) {
+            router.get().transitionTo('setup');
+            setupServerActions.started({started: true});
+            this.simulateProgress(60);
+            try {
+              await machine.rm();
+            } catch (err) {}
+            await machine.create();
+          } else {
+            let state = await machine.status();
+            if (state !== 'Running') {
+              if (state === 'Saved') {
+                router.get().transitionTo('setup');
+                this.simulateProgress(10);
+              } else if (state === 'Stopped') {
+                router.get().transitionTo('setup');
+                this.simulateProgress(25);
+              }
+              await machine.start();
+            }
+          }
+
+          // Try to receive an ip address from machine, for at least to 80 seconds.
+          let tries = 80;
+          while (!ip && tries > 0) {
+            try {
+              console.log('Trying to fetch machine IP, tries left: ' + tries);
+              ip = await machine.ip();
+              tries -= 1;
+              await Promise.delay(1000);
+            } catch (err) {}
+          }
         }
 
         if (ip) {
@@ -121,6 +113,8 @@ export default {
         } else {
           throw new Error('Could not determine IP from docker-machine.');
         }
+
+        if (util.isLinux()) { docker.ensureLinuxDockerIsRunning() }
 
         break;
       } catch (error) {
@@ -141,7 +135,6 @@ export default {
           'Machine Version': machineVersion,
           groupingHash: machineVersion
         }, 'info');
-
         this.clearTimers();
         await this.pause();
       }
